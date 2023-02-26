0a43cf1bf35d493c1a8ba6fbff3e3360801853ea
Harsha-Jagadish <harshajagadish99@gmail.com>
2023-02-26 04:14:11 -0800

    Added first image to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      {/*Header*/}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru"  }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+      </View>
+
+      <View>
+        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+        <Text className="font-bold text-xl">Current location</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
