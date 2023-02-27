42cae695d3bd81cefd049e95037934573a9ac91c
Harsha-Jagadish <harshajagadish99@gmail.com>
2023-02-26 22:14:41 -0800

    Fixed the header using tailwind css View wrapper

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,12 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+  ChevronDownIcon,
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -16,15 +22,22 @@ const HomeScreen = () => {
       {/*Header*/}
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
-          source={{ uri: "https://links.papareact.com/wru"  }}
+          source={{ uri: "https://links.papareact.com/wru" }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
+
+        <View>
+        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+      
+        <Text className="font-bold text-xl ">
+          Current location
+          <ChevronDownIcon size={20} color="00CCBB" />
+        </Text>
       </View>
 
-      <View>
-        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-        <Text className="font-bold text-xl">Current location</Text>
       </View>
+
+      
     </SafeAreaView>
   );
 };
